50d23fa415a1e37ef39773095b14122896a74f26
sduzair <sduzairahmed@gmail.com>
2022-03-22 05:11:54 -0400

    readonly g page

--- controllers/pageG.js
+++ controllers/pageG.js
@@ -1,5 +1,6 @@
 const Driver = require( "../models/Driver" )
 const bcrypt = require( "bcrypt" )
+const { redirect } = require( "express/lib/response" )
 
 module.exports = async function readDriverDetails( req, res ) {
   Driver.find( { userID: req.session.userId }, ( error, driver ) => {
@@ -12,14 +13,11 @@ module.exports = async function readDriverDetails( req, res ) {
     } else {
       bcrypt.compare( "_defaultinputCarLicenceNumber", driverObj.carLicenceNumber, ( err, same ) => {
         if( same ) {
-          return res.render( "driver/g_page", {
-            driver: true,
-            msg: "Pls enter new driver details"
-          } )
+          return res.redirect( "/driver/g2_page" )
         } else {
           return res.render( "driver/g_page", {
             driver: driverObj,
-            msg: "Driver found. You can update the details",
+            msg: "Driver found.",
           } )
         }
       } )
