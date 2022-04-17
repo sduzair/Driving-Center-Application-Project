146aa09c2767c371c93a73236f76e0188718e73f
sduzair <sduzairahmed@gmail.com>
2022-04-17 17:34:53 -0400

    appointment ui

--- controllers/pageAppointments.js
+++ controllers/pageAppointments.js
@@ -1,20 +1,25 @@
-const Driver = require("../models/Driver");
+const Driver = require( "../models/Driver" )
 module.exports = function( req, res ) {
 
-    //appointment not null and testResult not declared
-  Driver.find({appointmentID: {$ne:null}, testResult: null}, "-carMake -carModel -carYear -carLicenceNumber -DOB -userID -address",)
-      .populate("appointmentID", {match: {isTimeSlotAvailable: false}})
-      .exec((error, driverObj) => {
-        if (error || !driverObj || driverObj.length === 0) {
-          return res.status(200).json({"message": "Driver not found"})
-        }
-          res.status(404).json(driverObj)
-          // res.render("examiner/appointments", {
-          //     errors: null,
-          //     serverMsgs: null,
-          //     driverObj: driverObj,
-          // });
+  //appointment not null and testResult not declared
+  Driver.find( { appointmentID: { $ne: null }, testResult: null }, " firstName lastName appointmentType carMake carModel carPlatNumber image1 userID", )
+    .populate( "appointmentID", { match: { isTimeSlotAvailable: false } } )
+    .exec( ( error, driversObj ) => {
+      console.log( driversObj )
+      if( error || !driversObj || driversObj.length === 0 ) {
+        res.render( "examiner/appointments", {
+          errors: [ "error retrieving driver appointments" ],
+          serverMsgs: null,
+          driversObj: null,
+        } )
+      }
+      // res.status( 404 ).json( driversObj )
+      res.render( "examiner/appointments", {
+        errors: null,
+        serverMsgs: null,
+        driversObj: driversObj,
+      } )
 
-      })
+    } )
 
 }
