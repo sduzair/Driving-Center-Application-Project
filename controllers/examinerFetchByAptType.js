6dba7dfcb35b6db03e575c076e9729485ed37240
AZEEZ.AZIIZ <azeez.aziiz@gmail.com>
2022-04-17 19:17:52 -0400

    azeez-commit- getting only those drivers test results not released yet.

--- controllers/examinerFetchByAptType.js
+++ controllers/examinerFetchByAptType.js
@@ -2,7 +2,7 @@ const Driver = require( "../models/Driver" )
 
 module.exports = function( req, res ) {
     const { filterType } = req.params
-    Driver.find( { appointmentType: filterType, appointmentID: { $ne: null } } )
+    Driver.find( { appointmentType: filterType, appointmentID: { $ne: null }, testResult: null } )
         .populate( "appointmentID", { match: { isTimeSlotAvailable: false } } )
         .exec( ( error, driversObj ) => {
             if( error || !driversObj || driversObj.length === 0 ) {
@@ -13,14 +13,14 @@ module.exports = function( req, res ) {
                     filteredBy: filterType,
                 } )
             }
-            // res.status( 200 ).json( driversObj )
-            res.render( "examiner/appointments", {
+            res.status( 200 ).json( driversObj )
+            /*res.render( "examiner/appointments", {
                 errors: null,
                 serverMsgs: null,
                 driversObj: driversObj,
                 filteredBy: filterType,
             } )
-
+*/
         } )
 }
 
